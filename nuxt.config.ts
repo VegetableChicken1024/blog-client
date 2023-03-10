f72632d32eaf2e94ab9659f409ba0b03c9c9efdf
hbh <578092215@qq.com>
2023-03-10 14:24:47 +0800

    feat(all): less && commonCss 新增less，新增commonCss

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,8 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  modules: ['@pinia/nuxt']
+  modules: ['@pinia/nuxt'],
+  css: [
+    '~/assets/styles/common.less',
+    '~/assets/styles/variables.less'
+  ]
 })
