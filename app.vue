c83da246553462aa8958eccd40b9c7cdce3631b6
hbh <578092215@qq.com>
2023-03-10 11:19:46 +0800

    feat(all): 初始化项目

---
+++ app.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <NuxtWelcome />
+  </div>
+</template>
+
+<script setup lang="ts">
+import { ref, onMounted } from 'vue'
+const a = ref(1)
+onMounted(() => {
+  console.log(a.value, '123')
+})
+</script>
