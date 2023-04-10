f3b6a870ddd268ec4296865c1aa533bcb74ce0b4
huangbh <578092215@qq.com>
2023-04-10 19:11:37 +0800

    feat(component): 新增ElementPlus支持

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,9 +1,10 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  modules: ['@pinia/nuxt', '@pinia-plugin-persistedstate/nuxt'],
+  modules: ['@pinia/nuxt', '@pinia-plugin-persistedstate/nuxt', '@element-plus/nuxt'],
   css: [
     '~/assets/styles/common.less',
     '~/assets/styles/variables.less',
     '~/assets/styles/markdown.less'
-  ]
+  ],
+  elementPlus: { /** Options */ }
 })
